d4be454583eca2a466074543761829b6c12816ec
Artem <kaartem0402@gmail.com>
2023-02-18 10:45:40 +0100

    add 'player1'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Cheracter:
+class Character:
     name = ''
     health = 100
     damage = 1
@@ -6,3 +6,11 @@ class Cheracter:
 
     def set_name(self, new_name):
         self.name = new_name
+
+
+player1 = Character()
+
+print(player1.health)
+
+player1.set_name('Ameba')
+print(player1.name)
