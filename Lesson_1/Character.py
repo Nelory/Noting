38d8b6a1437c49839125a19b171e2b72f5de5ad3
Artem <kaartem0402@gmail.com>
2023-02-19 11:27:28 +0100

    add new file

--- Lesson_1/Character.py
+++ Lesson_1/Character.py
@@ -6,11 +6,3 @@ class Character:
 
     def set_name(self, new_name):
         self.name = new_name
-
-
-player1 = Character()
-
-print(player1.health)
-
-player1.set_name('Ameba')
-print(player1.name)
---
+++ Lesson_1/main.py
@@ -0,0 +1,8 @@
+from Character import Character
+
+player1 = Character()
+
+print(player1.health)
+
+player1.set_name('Ameba')
+print(player1.name)
