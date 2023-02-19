ddc0ef3567a2d179b2141f22fd8cd9a7426a3b9e
Artem <kaartem0402@gmail.com>
2023-02-19 12:02:41 +0100

    def str was addet

--- Lesson_1/character.py
+++ Lesson_1/character.py
@@ -9,5 +9,13 @@ class Character:
         self.health = health
         self.damage = damage
         self.defence = defence
-    def set_name(self, new_name):
+
+    def __str__(self):
+        return \
+            f'Name: {self, name}\n' \
+            f'Health: {self, health}\n' \
+            f'Damage: {self, damage}\n' \
+            f'Defence: {self, defence}\n'
+
+                def set_name(self, new_name):
         self.name = new_name
