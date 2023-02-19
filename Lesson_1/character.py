c9d0270ad532fdd273cdd20564dd3aec91acf106
Artem <kaartem0402@gmail.com>
2023-02-19 11:52:06 +0100

    bag fix

--- Lesson_1/character.py
+++ Lesson_1/character.py
@@ -2,9 +2,9 @@ class Character:
     name = ''
     health = 100
     damage = 1
-    defence = 1
+    defence = 0
 
-    def __int__(self, name='', health=100, damage=1, defence=0):
+    def __init__(self, name='', health=100, damage=1, defence=0):
         pass
     def set_name(self, new_name):
         self.name = new_name
