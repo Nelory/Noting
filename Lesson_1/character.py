ac076a19e7c59ca19f8f5e8423e32a72ad568555
Artem <kaartem0402@gmail.com>
2023-02-19 11:55:24 +0100

    fix Init

--- Lesson_1/character.py
+++ Lesson_1/character.py
@@ -5,6 +5,9 @@ class Character:
     defence = 0
 
     def __init__(self, name='', health=100, damage=1, defence=0):
-        pass
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
     def set_name(self, new_name):
         self.name = new_name
