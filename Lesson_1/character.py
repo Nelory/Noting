4cb1e6b8a639dc3ba90916cc7891d32c730734e3
Artem <kaartem0402@gmail.com>
2023-02-25 10:01:05 +0100

    fix

--- Lesson_1/character.py
+++ Lesson_1/character.py
@@ -33,5 +33,3 @@ class Character:
     def get_age(self, new_age):
         self.age = new_age
 
-    def is_alive(self):
-        return self.health > 0
--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -7,11 +7,10 @@ Student = Student(name='Anton', health=150,damage=10000, age=2004, group=11, ave
 print(f' - Player1 - \n{player1}')
 print(f' - student.py - \n{Student}')
 
-while player1.is_alive() and Anton.is_alive():
-    player1.attack(Student)
-    print('Player 1  attacked  Player 2')
-    Student.attack(player1)
-    print('student.py  attacked  Player 1')
+player1.attack(Student)
+print('Player 1  attacked  Player 2')
+Student.attack(player1)
+print('student.py  attacked  Player 1')
 
-    print(f' - Player1 - \n{player1}')
-    print(f' - student.py - \n{Student}')
+print(f' - Player1 - \n{player1}')
+print(f' - student.py - \n{Student}')
